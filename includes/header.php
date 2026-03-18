c22c9bff56b6b3ac1e488d69e2b5763945d55af1
ameliapolak <23362995@studentmail.ul.ie>
2026-03-18 18:16:57 +0000

    Updated navbar styles and colours,added some basic structure + need to add logo ,change sizing -->to be continued

--- includes/header.php
+++ includes/header.php
@@ -14,3 +14,35 @@ session_start();
   <link rel="stylesheet" href="/assets/css/style.css">
 </head>
 <body>
+<nav class="navbar navbar-expand-lg border-bottom">
+  <div class="container-fluid px-4">
+
+    <!-- CONTACT FAQ -->
+    <div class="d-flex gap-3">
+      <a class="nav-link" href="/pages/contact.php">CONTACT</a>
+      <a class="nav-link" href="/pages/faq.php">FAQ</a>
+    </div>
+
+    <!-- MINDLINK + add logo -->
+    <a class="navbar-brand mx-auto" href="/index.php">MINDLINK </a>
+
+    <!-- logged in vs logged out -->
+    <div class="d-flex gap-3 align-items-center">
+      <?php if (isset($_SESSION['user_id'])): ?>
+        <a class="nav-link" href="/pages/dashboard.php">Home</a>
+        <a class="nav-link" href="/pages/projects.php">Projects</a>
+        <a class="nav-link" href="/pages/profile.php">Profile</a>
+        <a class="nav-link" href="/pages/matches.php">Matches</a>
+        <a class="nav-link" href="/pages/settings.php">Settings</a>
+        <?php if (isset($_SESSION['role']) && $_SESSION['role'] === 'admin'): ?>
+          <a class="nav-link" href="/pages/admin.php">Admin</a>
+        <?php endif; ?>
+        <a href="/pages/logout.php" class="btn btn-dark rounded-pill px-4">LOG OUT</a>
+      <?php else: ?>
+        <a class="nav-link" href="/pages/login.php">LOG IN</a>
+        <a href="/pages/register.php" class="btn btn-signup">SIGN UP</a>
+      <?php endif; ?>
+    </div>
+
+  </div>
+</nav>
