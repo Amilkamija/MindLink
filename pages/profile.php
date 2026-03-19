5aea4056fd38fa91eb2b3190ce4a5eda12c487ff
Jakub Bolka <23378247@studentmail.ul.ie>
2026-03-19 13:55:54 +0000

    fix CSS stylesheet path

--- pages/profile.php
+++ pages/profile.php
@@ -9,7 +9,7 @@
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="assets/css/style.css">
+  <link rel="stylesheet" href="/assets/css/style.css">
 </head>
 <body>
   <div class="container-fluid px-4 px-lg-5 pt-3">
