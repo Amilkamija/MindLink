76d94fcfc3b070101caf6e0060375d025ce7ec8b
Jakub Bolka <23378247@studentmail.ul.ie>
2026-03-20 20:10:47 +0000

    Update admin page css path

--- pages/admin.php
+++ pages/admin.php
@@ -12,8 +12,6 @@ if ($conn->connect_error) {
 }
 ?>
 
-<?php require_once __DIR__ . '/includes/header.php'; ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -23,7 +21,7 @@ if ($conn->connect_error) {
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="/assets/css/style.css">
+  <link rel="stylesheet" href="../assets/css/admin.css">
 </head>
 <body>
   <div class="container-fluid px-4 px-lg-5 pt-3">
@@ -37,14 +35,14 @@ if ($conn->connect_error) {
       <div class="col-lg-4 text-center mb-3 mb-lg-0">
         <div class="logo-wrap">
           <h1 class="logo-text">MINDLINK</h1>
-          <img src="assets/img/mindlink_logo.png" alt="MindLink logo" class="logo-image">
+          <img src="../assets/img/mindlink_logo.png" alt="MindLink logo" class="logo-image">
         </div>
       </div>
 
       <div class="col-lg-5 d-flex justify-content-lg-end align-items-center gap-3 flex-wrap">
         <div class="search-pill">
           <span>SEARCH</span>
-          <img src="assets/img/search_icon.png" class="search-icon" alt="Search">
+          <img src="../assets/img/search_icon.png" class="search-icon" alt="Search">
         </div>
         <button class="logout-btn">LOG OUT</button>
       </div>
@@ -127,7 +125,7 @@ if ($conn->connect_error) {
               <div class="admin-search-box">Search users...</div>
               <div class="admin-filter-box">
                 <span>Filter: Active</span>
-                <img src="assets/img/search_icon.png" class="admin-small-search" alt="Search">
+                <img src="/assets/img/search_icon.png" class="admin-small-search" alt="Search">
               </div>
             </div>
 
@@ -247,5 +245,3 @@ if ($conn->connect_error) {
   </div>
 </body>
 </html>
-
-<?php require_once __DIR__ . '/includes/footer.php'; ?>
