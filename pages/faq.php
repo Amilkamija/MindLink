77f9272d8cc624ab39c965756e877c214f90bcee
Jakub Bolka <23378247@studentmail.ul.ie>
2026-03-20 20:13:53 +0000

    Fix faq page css path

--- pages/faq.php
+++ pages/faq.php
@@ -1,4 +1,3 @@
-<?php require_once __DIR__ . '/includes/header.php'; ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -9,7 +8,7 @@
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="/assets/css/style.css">
+  <link rel="stylesheet" href="/assets/css/faq.css">
 </head>
 <body>
   <div class="container-fluid px-4 px-lg-5 pt-3">
@@ -22,9 +21,11 @@
 
       <div class="col-lg-4 text-center mb-3 mb-lg-0">
         <div class="logo-wrap">
-          <h1 class="logo-text">MINDLINK</h1>
-          <img src="assets/img/mindlink_logo.png" alt="MindLink logo" class="logo-image">
-        </div>
+  <a href="/index.php" class="logo-link">
+    <h1 class="logo-text">MINDLINK</h1>
+  </a>
+  <img src="/assets/img/mindlink_logo.png" alt="MindLink logo" class="logo-image">
+</div>
       </div>
 
       <div class="col-lg-5 d-flex justify-content-lg-end align-items-center gap-3 flex-wrap">
@@ -73,5 +74,3 @@
   </div>
 </body>
 </html>
-
-<?php require_once __DIR__ . '/includes/footer.php'; ?>
