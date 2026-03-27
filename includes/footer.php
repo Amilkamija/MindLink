bab74c67131fdadf6f5fe7d0c9e23ae013f0e8ee
Amelia Polak <166129881+Amilkamija@users.noreply.github.com>
2026-03-27 02:19:32 +0000

    Fix footer.php
    Removed merge conflict markers from footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
-=======
 </div>
 </div>
->>>>>>> 7348a9a (Shared sidebar)
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
